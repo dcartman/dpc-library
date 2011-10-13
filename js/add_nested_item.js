3c803beaf3ba159d26093c38be7af9efcdd80493
Daniel Carter <daniel.carter@ddotb.com>
2011-10-13 13:40:51 -0400

    Initial Commit
    I decided to start added some of the css, js, php, and ruby objects I
    write and reuse a lot to git. Easier to track and backup.

---
+++ js/add_nested_item.js
@@ -0,0 +1,61 @@
+/**
+ * User: danielcarter
+ * Date: 10/12/11
+ * Time: 3:46 PM
+ * This object manages Adding and Building nested items.
+ * TODO:: Manage the removal of items?
+ * NOTE:: I'm not sure if this object should also handle the removal of nested items.
+ *        I think the nested Item should handle it's own removal. However if I make this
+ *        the parent object of all nested items and rename this to NestedItem having remove
+ *        here.
+ *        However it then defeats it's purpose as a button for nested items.
+ *        Maybe I should create Nest object?
+ */
+
+/**
+ * @param ele DOMElement
+ * @param $h  -- { nest: DOMElement, nestedClass: string, template: string }
+ */
+function AddNestedItem(ele, $h) {
+    var me = this,
+        $h = $h || {},
+        selector = this.selector || 'input, textarea';
+    this.element = ele || document.createElement("span");
+    this.element.object = this;
+
+    for (var p in $h) {
+        this[p] = $h[p];
+    }
+
+    this.nest = this.nest || "";
+    this.nestedClass = this.nestedClass || "nested-item";
+    this.template = this.template || "";
+
+    this.element.addEventListener("click", this.add, false);
+
+}
+
+AddNestedItem.prototype.add = function(evt){
+    var ele = (evt.currentTarget) ? evt.currentTarget : evt.srcElement,
+        me = ele.object,
+        new_item = me.createItem();
+    $(me.nest).append(new_item);
+}
+
+AddNestedItem.prototype.createItem = function() {
+    var me = this,
+        new_id = me.countNest();
+    return me.decodeHTML(me.template.replace(/NEW_RECORD/g, new_id));
+}
+
+AddNestedItem.prototype.countNest = function() {
+    var me = this;
+    return me.nest.getElementsByClassName(me.nestedClass).length;
+}
+
+AddNestedItem.prototype.decodeHTML = function(s) {
+    var s = s || this.template,
+        g = document.createElement('span');
+    g.innerHTML = s;
+    return g.firstChild.nodeValue;
+}
